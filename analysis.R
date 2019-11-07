10c8baa66d5a77bbe4cc52a588f5a6757b5a4807
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-11-07 13:46:15 -0500

    add data

--- analysis.R
+++ analysis.R
@@ -8,6 +8,6 @@ t <- read_tsv("data/tweets.tsv")
 t$status_id <- str_split(t$status_url, "/") %>% 
   map_chr(~ .[[6]])
 
-t <- rtweet::lookup_statuses(t$status_id, token = token)
+s <- lookup_statuses(t$status_id, token = token)
 
-write_csv(t, "processed-data.csv")
+write_rds(s, "processed-data.rds")
