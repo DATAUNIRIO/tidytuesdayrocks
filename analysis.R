6ae04ef4e51e0f73ea1cdbc0c93b90b8285c1d9e
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-11-07 13:08:09 -0500

    update

--- analysis.R
+++ analysis.R
@@ -2,10 +2,12 @@
 
 library(tidyverse)
 library(rtweet)
-c
+
 datasets <- read_tsv("data/datasets.tsv")
 t <- read_tsv("data/tweets.tsv")
 t$status_id <- str_split(t$status_url, "/") %>% 
   map_chr(~ .[[6]])
 
 t <- rtweet::lookup_statuses(t$status_id, token = token)
+
+write_csv(t, "processed-data.csv")
